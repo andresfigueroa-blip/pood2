bec3211e79cb5f9f9170c1e78a9fdd89948df943
ALUMNO# <ALUMNO#@ALUMNO-25>
2025-08-21 10:47:00 -0500

    Archivo grupo 4

---
+++ src/main/java/pood2/taller1/Taller1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pood2.taller1;
+
+/**
+ *
+ * @author ALUMNO#
+ */
+public class Taller1 {
+
+    public static void main(String[] args) {
+        
+        int num1=35 , num2=70 , resta, multi, suma;
+        resta = num1 - num2;
+        multi = num1 * num2;
+        suma = num1 + num2;
+                
+        System.out.println("Un rectangulo tiene de altura 35cm y de base 70cm entonces su area es de "+multi);
+        System.out.println("Y su perimetro es de "+suma);
+    }
+}
